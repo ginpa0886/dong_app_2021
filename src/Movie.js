d36bba28ce253beea63b80b8d3a073a87fdbd12a
Gomdi <ginpa0886@gmail.com>
2021-01-13 17:51:45 +0900

    #4.2 Styling the Movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ProrpTypes from 'prop-types';
+import "./Movie.css";
 
 function Movie({id, year, title, summary, poster }) {
-  return <h5>{title}</h5>
+  return (
+    <div class="moves">
+        <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title" style={{backgroundColor: "red"}}>{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>)
 }
 
 Movie.prorpTypes = {
